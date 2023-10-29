203e4b41bf415925f1c296850e09290d41469838
davidmaisuradze <maisuradze.davit@kiu.edu.ge>
2023-10-29 22:38:30 +0400

    now you can list students by school

--- university management system/src/main/java/com/davidmaisuradze/ums/controller/SchoolController.java
+++ university management system/src/main/java/com/davidmaisuradze/ums/controller/SchoolController.java
@@ -1,9 +1,14 @@
 package com.davidmaisuradze.ums.controller;
 
+import com.davidmaisuradze.ums.entity.School;
+import com.davidmaisuradze.ums.entity.Student;
 import com.davidmaisuradze.ums.service.SchoolService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 @Controller
 public class SchoolController {
@@ -17,6 +22,16 @@ public class SchoolController {
     public String getAllSchools(Model model) {
         model.addAttribute("schools", schoolService.getAllSchool());
         return "schools";
+    }
+
+    @GetMapping("/schools/{id}")
+    public String studentsOfSchool(@PathVariable int id, Model model) {
+        School school = schoolService.getSchoolById(id);
+        List<Student> students = school.getStudents();
+        model.addAttribute("students", students);
+        model.addAttribute("school", school);
+        model.addAttribute("schoolName", school.getName());
+        return "schoolstudent";
     }
 
 }
