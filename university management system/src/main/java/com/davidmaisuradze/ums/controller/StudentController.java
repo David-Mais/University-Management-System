a131019c53338772a5149be577cfc7385d5afa1d
davidmaisuradze <maisuradze.davit@kiu.edu.ge>
2023-11-08 11:19:23 +0400

    student select page added

--- university management system/src/main/java/com/davidmaisuradze/ums/controller/StudentController.java
+++ university management system/src/main/java/com/davidmaisuradze/ums/controller/StudentController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class StudentController {
@@ -18,5 +19,14 @@ public class StudentController {
     public String showStudentInfo(@PathVariable int id, Model model) {
         model.addAttribute("student", studentService.getStudentById(id));
         return "student";
+    }
+
+    @GetMapping("/studentselectpage")
+    public String selectStudent() {
+        return "studentselectpage";
+    }
+    @GetMapping("/student/redirect")
+    public String redirectToStudentProfile(@RequestParam(name = "id") int studentId) {
+        return "redirect:/student/" + studentId;
     }
 }
